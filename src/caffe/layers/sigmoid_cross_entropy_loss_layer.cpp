57d925cfa63d9d73c70b952cb9c2748aff40c8ad
ZHAO KAI <zeakey@126.com>
2016-09-21 22:37:14 +0800

    bug fix

--- src/caffe/layers/sigmoid_cross_entropy_loss_layer.cpp
+++ src/caffe/layers/sigmoid_cross_entropy_loss_layer.cpp
@@ -56,11 +56,11 @@ void SigmoidCrossEntropyLossLayer<Dtype>::Forward_cpu(
       for (int j = 0; j < dim; j ++) {
          if (target[i*dim+j] != 0) {
           count_pos ++;
-          temp_loss_pos -= input_data[i*dim + j] * (Dtype(target[i*dim+j]!=0) - (input_data[i*dim + j] >= 0)) -
+          temp_loss_pos -= input_data[i*dim + j] * ((target[i*dim+j]!=0) - (input_data[i*dim + j] >= 0)) -
                   log(1 + exp(input_data[i*dim + j] - 2 * input_data[i*dim + j] * (input_data[i*dim + j] >= 0)));
         } else {
           count_neg ++;
-          temp_loss_neg -= input_data[i*dim + j] * (Dtype(target[i*dim+j]!=0) - (input_data[i*dim + j] >= 0)) -
+          temp_loss_neg -= input_data[i*dim + j] * ((target[i*dim+j]!=0) - (input_data[i*dim + j] >= 0)) -
                   log(1 + exp(input_data[i*dim + j] - 2 * input_data[i*dim + j] * (input_data[i*dim + j] >= 0)));
         }
       } 
@@ -86,7 +86,9 @@ void SigmoidCrossEntropyLossLayer<Dtype>::Backward_cpu(
     const Dtype* sigmoid_output_data = sigmoid_output_->cpu_data();
     const Dtype* target = bottom[1]->cpu_data();
     Dtype* bottom_diff = bottom[0]->mutable_cpu_diff();
-    caffe_sub(count, sigmoid_output_data, target, bottom_diff);
+    //caffe_sub(count, sigmoid_output_data, target, bottom_diff);
+    for (int i = 0; i < count; ++i)
+      bottom_diff[i] = sigmoid_output_data[i] - (target[i]!=0);
     Dtype count_pos = 0;
     Dtype count_neg = 0;
     int dim = bottom[0]->count() / bottom[0]->num();
